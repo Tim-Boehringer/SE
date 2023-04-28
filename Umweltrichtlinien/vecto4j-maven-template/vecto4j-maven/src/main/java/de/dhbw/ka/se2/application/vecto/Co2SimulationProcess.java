a16b8ad8a30c19445c476f266efc3182ebd6651e
timborino <83162987+Tim-Boehringer@users.noreply.github.com>
2023-04-28 16:27:33 +0200

    Update Co2SimulationProcess.java
    Upodated the functionality of the class

--- Umweltrichtlinien/vecto4j-maven-template/vecto4j-maven/src/main/java/de/dhbw/ka/se2/application/vecto/Co2SimulationProcess.java
+++ Umweltrichtlinien/vecto4j-maven-template/vecto4j-maven/src/main/java/de/dhbw/ka/se2/application/vecto/Co2SimulationProcess.java
@@ -1,13 +1,22 @@
 package de.dhbw.ka.se2.application.vecto;
 
+import java.util.List;
+
+import de.dhbw.ka.se2.adapter.vehicledata.VehicleComponentDecoder;
 import de.dhbw.ka.se2.domain.logistics.VehicleWeights;
 import de.dhbw.ka.se2.domain.print.FullVehicle;
 import de.dhbw.ka.se2.domain.print.VehicleConfiguration;
+import de.dhbw.ka.se2.domain.vehicledata.VehicleComponent;
+import de.dhbw.ka.se2.plugin.vehicle.VehicleClient;
+import de.dhbw.ka.se2.application.vecto.VehicleWeightsClient;
 import de.dhbw.ka.se2.vecto4j.input.VehicleInput;
 import de.dhbw.ka.se2.vecto4j.input.VehicleType;
+import de.dhbw.ka.se2.vecto4j.Simulator;
+import de.dhbw.ka.se2.vecto4j.output.ElectricVehicleSimulationResult;
+
 
 public class Co2SimulationProcess {
-    private final WeightsClient weightsAccess;
+    private final VehicleWeightsClient weightsAccess;
     private final Simulator simulator;
     public Co2SimulationProcess(final VehicleWeightsClient weightsAccess, final Simulator simulator) {
         this.weightsAccess = weightsAccess;
@@ -31,10 +40,13 @@ public class Co2SimulationProcess {
         }
         throw new IllegalArgumentException("Unknown type " + vehicle.getMetadata().getType());
     }
-    private void enrichInputWithComponents(final VehicleConfiguration config, final VehicleInput input){
-
+    private void enrichInputWithComponents(final VehicleConfiguration vc, final VehicleInput input){
+        VehicleComponentDecoder vcd = new VehicleComponentDecoder();
+        VehicleClient vClient = new VehicleClient();
+        List<VehicleComponent> components = vClient.getComponents(vc);
+        vcd.decodeAndAddComponents(components, input);
     }
-    private void enrichInputWithWeights(){
-
+    private void enrichInputWithWeights(VehicleConfiguration vc, VehicleInput input){
+        input.setWeight(weightsAccess.getWeights(vc).getWeight());
     }
 }
